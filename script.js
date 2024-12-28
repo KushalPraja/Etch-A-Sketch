50cda9ee019b131511cb60977d7ed55531fb9e6c
kushal <kushpraja6@gmail.com>
2024-12-28 17:01:56 +0530

    added colors and saturation

--- script.js
+++ script.js
@@ -1,20 +1,89 @@
+const container= document.querySelector(".container");
+
+for (let i=0;i<256;i++){
+  const squares= document.createElement("div"); 
+  squares.classList.add("square");
+  container.appendChild(squares);
 
 
+}
+draw();
 
-document.addEventListener("DOMContentLoaded", function() {
-
+function promptMe(){
+  var pixel=prompt("How many pixels u want:");
+  var squares=document.getElementsByClassName("square");
+  console.log(squares.length);
+  for (let i = squares.length - 1; i >= 0; i--) {
+    squares[i].classList.remove("square");
+    console.log(i);
+  }
+  
+  console.log("reached");
   const container= document.querySelector(".container");
-
-  for (let i=0;i<256;i++){
+  for (let i=0;i<(pixel*pixel);i++){
     const squares= document.createElement("div"); 
-    squares.classList.add("square");
+    squares.className="square";
+    console.log("reached");
     container.appendChild(squares);
   }
 
+  var length=500/pixel;
+  var width=500/pixel;
+
+  squares.forEach(square=> {
+    
+    square.style.width=`${length} px`;
+    square.style.height=`${length} px`;
+  });
+  draw();
+
+}
+
+
+function draw(){
   const divs = document.querySelectorAll(".square");
+  var colorArray = [
+    "#FF6633",
+    "#FFB399",
+    "#FF33FF",
+    "#FFFF99",
+    "#00B3E6",
+    "#E6B333",
+    "#3366E6",
+    "#999966",
+    "#809980",
+    "#E6FF80",
+    "#1AFF33",
+    "#999933",
+    "#FF3380",
+    "#CCCC00",
+    "#66E64D",
+    "#4D80CC",
+    "#FF4D4D",
+    "#99E6E6",
+    "#6666FF"
+  ];
+
   divs.forEach((square) => {
-    square.addEventListener("mouseover", function () {
-      square.style.backgroundColor = "#000000";
-    });
+  square.opacityIncrement = 0.1;
+  square.addEventListener("mouseover", function () {
+    // Use the custom property to track the opacity increment
+    ;
+    square.style.opacity = 0.1+square.opacityIncrement;
+
+    // Increment the opacity increment for this square
+    square.opacityIncrement += 0.1;
+  });
+
+  square.colorset=false;
+
+  square.addEventListener("mouseover",function(){
+
+    if (square.colorset==false){
+      square.style.backgroundColor = colorArray[Math.floor(Math.random() * colorArray.length)];
+      square.colorset=true;
+    }
+
+  });
   })
-}); 
+}
