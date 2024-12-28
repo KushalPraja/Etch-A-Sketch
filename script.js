b64a5b91c03dd06f9a782751701a713818110ca7
kushal <kushpraja6@gmail.com>
2024-12-28 14:54:14 +0530

    removed repetitive code

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const divs = document.querySelectorAll(".square");
 
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  const container= document.querySelector(".container");
+
+  for (let i=0;i<256;i++){
+    const squares= document.createElement("div"); 
+    squares.classList.add("square");
+    container.appendChild(squares);
+  }
+
+  const divs = document.querySelectorAll(".square");
   divs.forEach((square) => {
     square.addEventListener("mouseover", function () {
       square.style.backgroundColor = "#000000";
